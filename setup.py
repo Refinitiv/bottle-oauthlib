d0a9cff1c7561c730b6438a03e68ad280f617c89
Jonathan Huot <jonathan.huot@thomsonreuters.com>
2017-09-14 11:42:16 +0200

    Added requirements.txt deps

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
+from os import path
+import inspect
 
+
+with open(path.join(path.dirname(path.abspath(inspect.getfile(inspect.currentframe()))), "requirements.txt")) as fd:
+    dependencies = fd.read().split('\n')
 
 setup(
     name='bottle-oauthlib',
     version='1.0',
     description='Bottle OAuth2.0 OAuthLib server implementation',
+    author="Thomson Reuters",
+    author_email="EikonEdge.Infra-Dev@thomsonreuters.com",
     packages=['bottle_oauthlib'],
+    install_requires=dependencies,
+    tests_require=dependencies,
 )

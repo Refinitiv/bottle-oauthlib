38781ccce31f854b1a389581c1bc41e746d21015
Dmytro Havrylevych <Dmytro.Havrylevych@thomsonreuters.com>
2017-09-20 13:29:28 +0300

    submitting base CI struct

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 from setuptools import setup
-from os import path
+from os import path,getenv
 import inspect
 
+if getenv("PATCH_VERSION"):
+    patch = getenv("PATCH_VERSION")
+else:
+    patch = "1"
+
+packageVersion = ("1.0.{}".format(patch))
 
 with open(path.join(path.dirname(path.abspath(inspect.getfile(inspect.currentframe()))), "requirements.in")) as fd:
     dependencies = fd.read().split('\n')
 
 setup(
     name='bottle-oauthlib',
-    version='1.0',
+    version=packageVersion,
     description='Bottle OAuth2.0 OAuthLib server implementation',
     author="Thomson Reuters",
     author_email="EikonEdge.Infra-Dev@thomsonreuters.com",
     packages=['bottle_oauthlib'],
     install_requires=dependencies,
-    tests_require=dependencies,
+    #tests_require=dependencies,
 )

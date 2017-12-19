256e494851d35cbef90bbb1f5e6a954ccb49e3e2
Jonathan Huot <jonathan.huot@thomsonreuters.com>
2017-12-19 09:47:55 +0100

    Changed pypi description to be identical to github

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(path.dirname(path.abspath(inspect.getfile(inspect.currentfra
 setup(
     name='bottle-oauthlib',
     version='1.0.1',
-    description='Bottle plugin for OAuthLib framework (OAuth2.0)',
+    description='Bottle adapter for OAuthLib framework (OAuth2.0)',
     license='BSD-3-Clause',
     author="Thomson Reuters",
     author_email="EikonEdge.Infra-Dev@thomsonreuters.com",
